f8f52b0cf0e9d78955436522a1a79d7f1af6bcc7
Cirofim <cirofim8767@gmail.com>
2026-01-31 00:24:49 +0300

    header_hover

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,9 +3,91 @@
 </script>
 
 <template>
+  <header class="header">
+<div class="header_container-up">
+
+
+
+
+<div class="header_logo">
+  <img class="header_logo--img" width="220" height="51" src="" alt="Лого" >
+</div>
+
+
+<div class="header_wrapper">
+
+<a class="header_link--btn" href="" >
+  <div class="header_text--btn">Искать жильё</div>
+</a>
+
+<a class="header_link--btn" href="" >
+  <div class="header_text--btn">Блог</div>
+</a>
+
+<a class="header_link--btn" href="" >
+  <div class="header_text--btn">Частые вопросы</div>
+</a>
+
+<a href="" class="header_img--btn">
+<img src="./components/icons/bell.svg" alt="" class="header_icon bell_icon">
+</a>
+<a href="" class="header_img--btn">
+<img src="./components/icons/like.svg" alt="" class="header_icon like_icon">
+</a>
+
+
+<a class="header_link--btn header_large--btn" href="" >
+  <div class="header_text--btn header_large--text ">Мой аккаунт</div>
+</a>
+
+</div>
+
+</div>
+
+
+
+<div class="header_container-down">
+
+<div class="header_wrapper-big">
+
+  <a class="header_link--btn" href="" >
+  <div class="header_text--btn">Посуточная аренда</div>
+</a>
+<span class="header_text--btn">/</span>
+<a class="header_link--btn" href="" >
+  <div class="header_text--btn">Барановичи, Брестская область</div>
+</a>
+<span class="header_text--btn">/</span>
+<a class="header_link--btn" href="" >
+  <div class="header_text--btn">70 - 150 BYN</div>
+</a>
+<span class="header_text--btn">/</span>
+<a class="header_link--btn" href="" >
+  <div class="header_text--btn">27.09.25 - 30.09.25</div>
+</a>
+
+</div>
+
+<div class="header_wrapper--small">
+  
+  <a class="header_link--btn" href="" >
+    <div class="header_text--btn">Сохранить вариант</div>
+  </a>
+
+  <a href="" class="header_img--btn">
+    <img src="./components/icons/grey_like.svg"  alt="" class="header_icon like_icon">
+  </a>
+
+</div>
+
+</div>
+
+</header>
+
+
 
 </template>
 
 <style>
-
+@import url(./assets/blocks/header.css);
 </style>
